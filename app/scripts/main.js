f7de0c7166a84f6f8b432bfa434b888787483174
Mike Reinten <mikereinten@gmail.com>
2016-11-11 02:19:55 +0100

    feat(zuidafrika): first install and first app

---
+++ app/scripts/main.js
@@ -0,0 +1,438 @@
+var locations = [
+    {
+        "title": "O.R. Tambo International Airport",
+        "lat": "-26.1366728",
+        "lng": "28.2389572",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1504-airport-plane_4x.png",
+        "description": "O R Tambo Airport Rd, Johannesburg, 1627, Zuid-Afrika"
+    },
+    {
+        "title": "Protea Hotel Manor",
+        "lat": "-25.7505428",
+        "lng": "28.2308334",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Protea Hotel Manor, Pretoria, Gauteng, Zuid-Afrika"
+    },
+    {
+        "title": "Voortrekker Monument",
+        "lat": "-25.7763285",
+        "lng": "28.1736098",
+        "description": "Voortrekker Monument, Monument Street, Pretoria, Zuid-Afrika"
+    },
+    {
+        "title": "Kruger House Museum",
+        "lat": "-25.7464999",
+        "lng": "28.1795513",
+        "description": "Kruger House & Museum, WF Nkomo Street, Pretoria, Zuid-Afrika"
+    },
+    {
+        "title": "Misty Mountain",
+        "lat": "-25.17497",
+        "lng": "30.6575913",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Misty Mountain, Thaba Chweu, Mpumalanga, Zuid-Afrika"
+    },
+    {
+        "title": "Bourke's Luck Potholes",
+        "lat": "-24.6744049",
+        "lng": "30.8087596",
+        "description": "Bourke's Luck Potholes, Thaba Chweu, Mpumalanga, Zuid-Afrika"
+    },
+    {
+        "title": "Blyde River Canyon",
+        "lat": "-24.5822584",
+        "lng": "30.7625659",
+        "description": "Blyde River Canyon, Thaba Chweu, Mpumalanga, Zuid-Afrika"
+    },
+    {
+        "title": "Wonder View",
+        "lat": "-24.871182",
+        "lng": "30.8949179",
+        "description": "Wonder View, Thaba Chweu, Mpumalanga, Zuid-Afrika"
+    },
+    {
+        "title": "Pine Lake Resort",
+        "lat": "-25.280195",
+        "lng": "30.9965393",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Pine Lake Resort, Mbombela, Mpumalanga, Zuid-Afrika"
+    },
+    {
+        "title": "Phabeni Gate, Kruger National Park",
+        "lat": "-25.0251705",
+        "lng": "31.2389057",
+        "description": "Phabeni Gate, Kruger National Park, Kruger Park, Mpumalanga, Zuid-Afrika"
+    },
+    {
+        "title": "Pine Lake Resort",
+        "lat": "-25.280195",
+        "lng": "30.9965393",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Pine Lake Resort, Mbombela, Mpumalanga, Zuid-Afrika"
+    },    
+    {
+        "title": "Jeppes Reef Border Post",
+        "lat": "-25.7504297",
+        "lng": "31.4665228",
+        "description": "Jeppes Reef, Nkomazi, Mpumalanga, Zuid-Afrika"
+    },
+    {
+        "title": "Hotel Lugogo Sun",
+        "lat": "-26.409564",
+        "lng": "31.1749193",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Lugogo Sun, MR103, Lobamba, Swaziland"
+    },
+    {
+        "title": "Golela Border Post",
+        "lat": "-27.3179699",
+        "lng": "31.8863574",
+        "description": "Border Posts - Golela, MR8, Pongola, KwaZoeloe-Natal, Zuid-Afrika"
+    },
+    {
+        "title": "Hluhluwe–iMfolozi Park",
+        "lat": "-28.2198305",
+        "lng": "31.9496765",
+        "description": "Hluhluwe–iMfolozi Park, St Lucia Park, KwaZoeloe-Natal, Zuid-Afrika"
+    },
+    {
+        "title": "Elifant Lake Inn",
+        "lat": "-28.377518",
+        "lng": "32.4103103",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Elifant Lake Inn, Flamingo Street, Saint Lucia, Zuid-Afrika"
+    },
+    {
+        "title": "Durban",
+        "lat": "-29.8483609",
+        "lng": "30.9223192",
+        "icon": "",
+        "description": "Durban, KwaZoeloe-Natal, Zuid-Afrika"
+    },
+    {
+        "title": "Howick Falls",
+        "lat": "-29.4859411",
+        "lng": "30.2366495",
+        "description": "Howick Falls, Falls Drive, Howick, Zuid-Afrika"
+    },
+    {
+        "title": "Gooderson Drakensberg Gardens Golf & Spa Resort",
+        "lat": "-29.754906",
+        "lng": "29.2375973",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Gooderson Drakensberg Gardens Golf & Spa Resort, Drakensberg Gardens Road, Underberg, Zuid-Afrika"
+    },
+    {
+        "title": "Sani Pass Tours",
+        "lat": "-29.791648",
+        "lng": "29.4937232",
+        "description": "Sani Pass Tours, Underberg, KwaZoeloe-Natal, Zuid-Afrika"
+    },
+    {
+        "title": "Sani Mountain Lodge",
+        "lat": "-29.5845233",
+        "lng": "29.2861621",
+        "description": "Sani Mountain Lodge, Mkhomazi Wilderness area, Thaba-Tseka, Lesotho"
+    },
+    {
+        "title": "Gooderson Drakensberg Gardens Golf & Spa Resort",
+        "lat": "-29.754906",
+        "lng": "29.2375973",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Gooderson Drakensberg Gardens Golf & Spa Resort, Drakensberg Gardens Road, Underberg, Zuid-Afrika"
+    },
+    {
+        "title": "Umnenga Lodge",
+        "lat": "-32.8822743",
+        "lng": "28.0896486",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Umnenga Lodge, Glen Eden Road, Glen Gariff, East London, Zuid-Afrika"
+    },
+    {
+        "title": "Township Port Elizabeth",
+        "lat": "-33.8833941",
+        "lng": "25.5678932",
+        "description": "Kwazakele 3, Ibhayi, Zuid-Afrika"
+    },
+    {
+        "title": "Summerstrand Hotel",
+        "lat": "-33.9936815",
+        "lng": "25.6746307",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Summerstrand Hotel Port Elizabeth, Marine Drive, Port Elizabeth, Oost-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "Donkin Reserve - Hoogste vlaggenmast, Pyramide, Mozaiek",
+        "lat": "-33.962032",
+        "lng": "25.6180793",
+        "description": "Donkin Reserve, Port Elizabeth, Zuid-Afrika"
+    },
+    {
+        "title": "Cape St Francis Resort Hotel",
+        "lat": "-34.1989382",
+        "lng": "24.8293748",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Cape St Francis Resort Hotel, Da Gama Road, Cape Saint Francis, Zuid-Afrika"
+    },
+    {
+        "title": "The Big Tree Tsitsikamma",
+        "lat": "-33.96608",
+        "lng": "23.8931823",
+        "description": "The Big Tree Tsitsikamma, N2, Stormsrivier, Zuid-Afrika"
+    },
+    {
+        "title": "Storms River Mouth",
+        "lat": "-34.0203",
+        "lng": "23.9007713",
+        "description": "Storms River Mouth, Aberdeen Plain, Oost-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "Knysna - Catamaran",
+        "lat": "-34.0391361",
+        "lng": "23.007397",
+        "description": "Knysna, West-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "The Wilderness Hotel",
+        "lat": "-33.9930137",
+        "lng": "22.5748755",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "The Wilderness Hotel, George Road, Wildernis, Zuid-Afrika"
+    },
+    {
+        "title": "Paragliden",
+        "lat": "-33.96608",
+        "lng": "23.8931823",
+        "description": "Sedgefield, West-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "The Wilderness Hotel",
+        "lat": "-33.9930137",
+        "lng": "22.5748755",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "The Wilderness Hotel, George Road, Wildernis, Zuid-Afrika"
+    },
+    {
+        "title": "Safari Ostrich Show Farm",
+        "lat": "-33.630527",
+        "lng": "22.1594273",
+        "description": "Safari Ostrich Show Farm, R328, Oudtshoorn, Zuid-Afrika"
+    },
+    {
+        "title": "Cango Caves",
+        "lat": "-33.3923",
+        "lng": "22.2127113",
+        "description": "Cango Caves, Oudtshoorn, Zuid-Afrika"
+    },
+    {
+        "title": "Kobus se Gat",
+        "lat": "-33.38854",
+        "lng": "22.1052113",
+        "description": "Kobus se Gat (Back packers/Mt bikers rest), Zuid-Afrika"
+    },
+    {
+        "title": "Swartberg Pass",
+        "lat": "-33.3547222",
+        "lng": "22.049478",
+        "description": "Swartberg Pass, Oudtshoorn, West-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "Oudtshoorn Inn",
+        "lat": "-33.583867",
+        "lng": "22.2026893",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Oudtshoorn Inn, Van Der Riet Street, Oudtshoorn, Zuid-Afrika"
+    },
+    {
+        "title": "The Country Pumpkin",
+        "lat": "-33.9052293",
+        "lng": "20.7143545",
+        "description": "The Country Pumpkin, Barrydale, West-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "Robertson Winery",
+        "lat": "-33.8053442",
+        "lng": "19.8756131",
+        "description": "Robertson Winery, Robertson, Zuid-Afrika"
+    },
+    {
+        "title": "Garden Court Nelson Mandela Boulevard",
+        "lat": "-33.9347119",
+        "lng": "18.4456409",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Garden Court Nelson Mandela Boulevard, Kaapstad, Zuid-Afrika"
+    },
+    {
+        "title": "St George's Cathedral",
+        "lat": "-33.924845",
+        "lng": "18.4171633",
+        "description": "St George's Cathedral, Wale Street, Kaapstad, Zuid-Afrika"
+    },
+    {
+        "title": "Bo-Kaap",
+        "lat": "-33.9215342",
+        "lng": "18.4128168",
+        "description": "Bo-Kaap, Kaapstad, West-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "V&A Waterfront",
+        "lat": "-33.9068352",
+        "lng": "18.4200008",
+        "description": "V&A Waterfront, Kaapstad, Zuid-Afrika"
+    },
+    {
+        "title": "Garden Court Nelson Mandela Boulevard",
+        "lat": "-33.9347119",
+        "lng": "18.4456409",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Garden Court Nelson Mandela Boulevard, Kaapstad, Zuid-Afrika"
+    },
+    {
+        "title": "Tafelberg",
+        "lat": "-33.9628212",
+        "lng": "18.4010858",
+        "description": "Tafelberg, Kaapstad, West-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "Boulders Beach",
+        "lat": "-34.1972201",
+        "lng": "18.4490964",
+        "description": "Boulders Beach, Kleintuin Road, Zuid-Afrika"
+    },
+    {
+        "title": "Cape of Good Hope",
+        "lat": "-34.3568425",
+        "lng": "18.4717995",
+        "description": "Cape of Good Hope, Kaapstad, Zuid-Afrika"
+    },
+    {
+        "title": "Cape Point",
+        "lat": "-34.3568851",
+        "lng": "18.4967873",
+        "description": "Cape Point, Kaapstad, West-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "Garden Court Nelson Mandela Boulevard",
+        "lat": "-33.9347119",
+        "lng": "18.4456409",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Garden Court Nelson Mandela Boulevard, Kaapstad, Zuid-Afrika"
+    },
+    {
+        "title": "Long Street",
+        "lat": "-33.922015",
+        "lng": "18.4174353",
+        "description": "Long Street, Kaapstad, West-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "Duiker Island",
+        "lat": "-34.0585978",
+        "lng": "18.3246316",
+        "description": "Duiker Island, Hout Bay, Zuid-Afrika"
+    },
+    {
+        "title": "Camps Bay Beach",
+        "lat": "-33.9506695",
+        "lng": "18.3745891",
+        "description": "Camps Bay Beach, Kaapstad, West-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "Richard's Supper Stage and Bistro",
+        "lat": "-33.908712",
+        "lng": "18.3954171",
+        "description": "Richard's Supper Stage and Bistro, Main Road, Kaapstad, Zuid-Afrika"
+    },
+    {
+        "title": "Garden Court Nelson Mandela Boulevard",
+        "lat": "-33.9347119",
+        "lng": "18.4456409",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1602-hotel-bed_4x.png",
+        "description": "Garden Court Nelson Mandela Boulevard, Kaapstad, Zuid-Afrika"
+    },
+    {
+        "title": "Drakenstein Prison",
+        "lat": "-33.845273",
+        "lng": "19.009738",
+        "description": "Drakenstein Prison, Paarl, West-Kaap, Zuid-Afrika"
+    },
+    {
+        "title": "Stellenbosch",
+        "lat": "-33.9328078",
+        "lng": "18.8622583",
+        "description": "Stellenbosch, Zuid-Afrika"
+    },
+    {
+        "title": "Road Lodge Cape Town International Airport",
+        "lat": "-33.9698493",
+        "lng": "18.5897486",
+        "icon": "https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1504-airport-plane_4x.png",        
+        "description": "Road Lodge Cape Town International Airport, Kaapstad, Zuid-Afrika"
+    },
+    
+]
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -28.405, lng: 20.180},
+        zoom: 6
+    });
+
+    var infoWindow = new google.maps.InfoWindow();
+    var lat_lng = new Array();
+    var latlngbounds = new google.maps.LatLngBounds();
+
+    for (var i = 0; i < locations.length; i++) {
+        var data = locations[i];
+        var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+        lat_lng.push(myLatlng);
+        marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: data.title,
+            icon: data.icon
+        })
+        latlngbounds.extend(marker.position);
+        (function (marker, data) {
+                google.maps.event.addListener(marker, "click", function (e) {
+                infoWindow.setContent(data.description);
+                infoWindow.open(map, marker);
+            });
+        })(marker, data);
+    }
+    
+    map.setCenter(latlngbounds.getCenter());
+    map.fitBounds(latlngbounds);
+
+
+    // ROUTING 
+    //Initialize the Path Array
+    var path = new google.maps.MVCArray();
+
+    //Initialize the Direction Service
+    var service = new google.maps.DirectionsService();
+
+    //Set the Path Stroke Color
+    var poly = new google.maps.Polyline({ map: map, strokeColor: '#4986E7' });
+
+    //Loop and Draw Path Route between the Points on MAP
+    for (var i = 0; i < lat_lng.length; i++) {
+        if ((i + 1) < lat_lng.length) {
+            var src = lat_lng[i];
+            var des = lat_lng[i + 1];
+            //path.push(src);
+            poly.setPath(path);
+            service.route({
+                origin: src,
+                destination: des,
+                travelMode: google.maps.DirectionsTravelMode.DRIVING
+            }, function (result, status) {
+                if (status == google.maps.DirectionsStatus.OK) {
+                    for (var i = 0, len = result.routes[0].overview_path.length; i < len; i++) {
+                        path.push(result.routes[0].overview_path[i]);
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app',
+      index: 'map.html'
+    }
+  });
+
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+});
